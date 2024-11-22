be537a1e49392836d448796b0006bb8925b77621
Ayami2004 <anjananee.kothalawala@gmail.com>
2024-11-22 15:37:39 +0530

    Update connection.php

--- Fredrick's/connection.php
+++ Fredrick's/connection.php
@@ -1,7 +1,7 @@
 <?php
     $host = "localhost";
     $username = "root";
-    $password = "BibbidyBoo8999%";
+    $password = "...";
     $database = "fredricks";
     $port = "3306";
     $connection = new mysqli($host, $username, $password, $database, $port);
